0cda02ec2f9145e79445d4ce115dc1a717394a0d
felipeDosProgramas <111545218+felipeDosProgramas@users.noreply.github.com>
2023-08-09 10:24:07 -0300

    slids

--- script.js
+++ script.js
@@ -1,44 +1,54 @@
-let imgs = document.getElementsByTagName('img');
-let meio = document.getElementById('meio');
-let botao = document.getElementById('botao');
-let str = [];
-let x = 0;
-let intervalo;
+const slides = document.querySelectorAll('.carousel-slide');
+const stopButton = document.getElementById('stopButton');
+const resumeButton = document.getElementById('resumeButton');
+const progressBar = document.querySelector('.progress');
+let currentSlideIndex = 0;
+let intervalId;
+let progress = 0;
+const slideDuration = 17500; // Tempo em milissegundos para cada slide
 
-function voltar(){	
-	interval = setInterval(() => {	
-		if(str[++x]){
-			meio.innerHTML = str[x]
-			return;
-		}
-		x = -1
-	}, 8000)		
-	botao.removeEventListener('click', voltar)
-	botao.addEventListener('click',parar)
-	console.log(botao.ev)
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+    progress = 0;
+    progressBar.style.width = '0%';
 }
 
-function parar(){
-	clearInterval(interval)	
-	botao.removeEventListener('click',parar )
-	botao.addEventListener('click',voltar)
-	console.log(botao.onclick)
+function nextSlide() {
+    progress = -50;
+    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+    showSlide(currentSlideIndex);
 }
 
-str[0] = '<h2>O que é Acessibilidade Digital?</h2><p>Acessibilidade digital é a prática de garantir que pessoas com deficiência possam utilizar e interagir com tecnologias digitais, como sites, aplicativos e dispositivos eletrônicos.</p><img src="aceesivel.jpeg" alt="Pessoas com deficiência usando tecnologia" >';
-str[1] = '<section><h2>Por que é Importante?</h2><p>A acessibilidade digital é importante para promover a inclusão e igualdade de oportunidades para todas as pessoas, independentemente de suas limitações físicas.</p><img src="acesove.jpeg" alt="Tecnologias acessíveis" ></section>';
-str[2] = '<section><h2>Como Podemos Ajudar?</h2><p>Existem várias formas de tornar a tecnologia mais acessível, como a utilização de padrões de codificação acessíveis, fornecer descrições alternativas para imagens e disponibilizar recursos para leitores de tela. Como essa página onde o mouse é inútil!</p><img src="deficenca.jpg" alt="Ajudando pessoas com deficiência" ></section>';
-str[3] = '<section><h2>Como apliquei isso aqui?</h2><p>Para deficientes físicos, sites como excesso de interatividade podem ser um problema, por isso, desenvolvi utilizando um modelo de <i>apresentação de slides</i>, onde sequer é necessário clicar em algum link para acessar as informações!</p><img src="slides.jpg" alt="Apresentação de slides" ></section>';
-str[4] = '<section><h2>Apenas os manetas/pernetas são deficientes?</h2><p>Por exemplo, quando você esta com as mão ocupadas, mas prescisa acessar algum tipo de dado/informação, naquele momento você se torna elegível a utilizar os recursos que a acessibilidade para <i>manetas</i> pode oferecer!</p><img src="semao.jpg" alt="Apresentação de slides" ></section>';
+function startCarousel() {
+    intervalId = setInterval(updateProgress, 50);
+}
 
+function stopCarousel() {
+    clearInterval(intervalId);
+}
 
-meio.innerHTML = str[0]
-interval = setInterval(() => {	
-	if(str[++x]){
-		meio.innerHTML = str[x]
-		return;
+function updateProgress() {
+    progress += 50;
+    const percentage = (progress / slideDuration) * 100;
+    progressBar.style.width = percentage + '%';
+	
+    if (progress >= slideDuration) {
+		nextSlide();
 	}
-	x = -1
-}, 8000)
+}
 
-botao.addEventListener('click', voltar)
+startCarousel();
+
+stopButton.addEventListener('click', () => {
+    stopCarousel();
+});
+
+resumeButton.addEventListener('click', () => {
+    startCarousel();
+});
+
+
+
+
+
